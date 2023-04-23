0797a0661c24cabf4e23bc7a04036dcc7379449b
Nikolay <nikolayrudakov@mail.ru>
2023-04-23 14:46:28 +0600

    finished work on homework 1, task 1

--- homework_1/src/task_1.java
+++ homework_1/src/task_1.java
@@ -1,2 +1,50 @@
+import java.util.Scanner;
+
 public class task_1 {
+    public static void main(String[] args) {
+        System.out.println("Triangular number.");
+        System.out.print("Enter triangular numbers quantity: ");
+        int limit_1 = (int) getData();
+        int triangularNumber;
+        for (int i = 1; i <= limit_1; i++) {
+            triangularNumber = 0;
+            for (int j = 0; j <= i; j++) {
+                triangularNumber += j;
+            }
+            System.out.print(triangularNumber + ", ");
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println("Factorial.");
+        int limit_2 = (int) getData();
+        long factorial = 1;
+        for (int i = 1; i <= limit_2; i++) {
+            factorial *= i;
+        }
+        System.out.println("Result: " + factorial);
+        System.out.println();
+        System.out.println("Simple numbers.");
+        System.out.print("Enter simple number calculation limit: ");
+        int limit_3 = (int) getData();
+        if (limit_3 < 2)
+            System.out.println("Not possible to calculate simple number");
+        for (int i = 2; i <= limit_3; i++) {
+            if (checkSimpleNumber(i))
+                System.out.print(i + ", ");
+        }
+    }
+
+    public static double getData() {
+        Scanner enteredData = new Scanner(System.in);
+        double dataForProcessing = enteredData.nextDouble();
+        return dataForProcessing;
+    }
+
+    public static boolean checkSimpleNumber(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0)
+                return false;
+        }
+            return true;
+    }
 }
