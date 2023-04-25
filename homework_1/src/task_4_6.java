486cf8a88d166ef352d6284fa05df5382efe9e2d
Nikolay <nikolayrudakov@mail.ru>
2023-04-25 11:59:19 +0600

    finished work on homework 1, task 4, subtask 7 (unique elements count).

--- homework_1/src/task_4_6.java
+++ homework_1/src/task_4_6.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Random;
 
-public class task_4 {
+public class task_4_6 {
     public static void main(String[] args) {
 
         System.out.println("Enter size, random numbers range (minimum & maximum)");
---
+++ homework_1/src/task_4_7.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class task_4_7 {
+    public static void main(String[] args) {
+        System.out.println("Enter list size, random numbers range (minimum & maximum)");
+        int listSize = (int) task_1.getData();
+        int minRandomValue = (int) task_1.getData();
+        int maxRandomValue = (int) task_1.getData();
+
+
+        List<Integer> initialList = makeList(listSize, minRandomValue, maxRandomValue);
+        System.out.println(initialList);
+        List<Integer> generatedUniqueElementsList = generateUniqueList(listSize, initialList);
+        System.out.println(generatedUniqueElementsList);
+        printListElements(generatedUniqueElementsList);
+    }
+
+    private static void printListElements(List<Integer> generatedUniqueElementsList) {
+        for (int value:
+                generatedUniqueElementsList) {
+            System.out.println(value);
+        }
+    }
+
+    private static List<Integer> generateUniqueList(int listSize, List<Integer> initialList) {
+        List<Integer> uniqueElementsList = new ArrayList<>();
+        int occurCounter = 0;
+        for (int i = 0; i < listSize; i++) {
+            int checkingElement = initialList.get(i);
+            for (int j = 0; j < listSize; j++) {
+                if (checkingElement == initialList.get(j)) {
+                    occurCounter += 1;
+                }
+            }
+            if (occurCounter == 1) {
+                uniqueElementsList.add(checkingElement);
+            }
+            occurCounter = 0;
+        }
+        return uniqueElementsList;
+    }
+
+    private static List<Integer> makeList(int listSize, int minRandomValue, int maxRandomValue) {
+        List<Integer> randomValuesList = new ArrayList<>();
+        for (int i = 0; i < listSize; i++) {
+            randomValuesList.add(randomizer(minRandomValue, maxRandomValue));
+        }
+        return randomValuesList;
+    }
+
+    private static int randomizer(int minimumValue, int maximumValue) {
+        Random randomValue = new Random();
+        return randomValue.nextInt(minimumValue, maximumValue);
+        }
+    }
