1ed667ffaaa40597ee6b227392e686e7b9ae4af2
Nikolay <nikolayrudakov@mail.ru>
2023-04-28 12:30:19 +0600

    Finished work on task 1, homework 2

--- homework_2/src/task_2_1.java
+++ homework_2/src/task_2_1.java
@@ -8,6 +8,17 @@ public class task_2_1 {
         String initialString = inputData.nextLine();
         int[] indexesArray = generateIndexesArray(initialString);
         System.out.println(Arrays.toString(indexesArray));
+        reverseAndPrintResult(initialString, indexesArray);
+    }
+
+    private static void reverseAndPrintResult(String initialString, int[] indexesArray) {
+        String reversedString = "";
+
+        for (int i = 0; i < indexesArray.length; i++) {
+              char letter = initialString.charAt(indexesArray[i] - 1);
+              reversedString += letter;
+        }
+        System.out.println(reversedString);
     }
 
     private static int[] generateIndexesArray(String initialString) {
