7cfb89df9bd31daf8f647d0e761900e2f4bd91f4
Nikolay <nikolayrudakov@mail.ru>
2023-05-08 17:16:41 +0600

    Finished work at task 1, homework 3

--- homework_3/src/task_1.java
+++ homework_3/src/task_1.java
@@ -1,2 +1,42 @@
+import java.util.*;
+
 public class task_1 {
+    public static void main(String[] args) {
+        int length = getData();
+        int minimum = getData();
+        int maximum = getData();
+
+        List<Integer> listOfRandomValues = generateRandomList(length, minimum, maximum);
+        System.out.println(listOfRandomValues);
+        removeEvenNumbers(listOfRandomValues);
+        System.out.println(listOfRandomValues);
+    }
+    public static Integer getData() {
+        Scanner entryData = new Scanner(System.in);
+        return entryData.nextInt();
+    }
+    public static List<Integer> generateRandomList(int length, int minimum, int maximum) {
+        List<Integer> randomList = new ArrayList<>();
+        Random randomizer = new Random();
+        for (int i = 0; i < length; i++) {
+            randomList.add(randomizer.nextInt(minimum, maximum));
+        }
+        return randomList;
+    }
+    public static void removeEvenNumbers(List<Integer> checkingList) {
+        checkingList.removeIf(integer -> integer % 2 == 0);
+    }
+
 }
+    /*
+        public static void removeEvenNumbers(List<Integer> checkingList) {
+        Iterator<Integer> scannedList = checkingList.iterator();
+        while (scannedList.hasNext()) {
+            if (scannedList.next() % 2 == 0) {
+                scannedList.remove();
+            }
+        }
+    }
+     */
+
+
