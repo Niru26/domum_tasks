5de18785bee8dcc2193cb927bfb7881dbc4be721
Nikolay <nikolayrudakov@mail.ru>
2023-05-09 10:41:15 +0600

    Test object

---
+++ homework_3/src/test_object.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class test_object {
+    public static void main(String[] args) {
+        Integer number = enterData();
+        String message = enterData();
+        Float rationalNumber = enterData();
+    }
+
+    private static Object enterData() {
+        Scanner entryData = new Scanner(System.in);
+        return entryData.next();
+    }
+}
